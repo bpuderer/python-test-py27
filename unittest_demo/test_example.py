863820ad5b1fbdf94060662444466332cd2d26b1
Ben Puderer <bpuderer@yahoo.com>
2016-02-07 14:40:19 -0500

    updated unittest demo to match behave demo and moved sims up a level

--- unittest_demo/test_example.py
+++ unittest_demo/test_example.py
@@ -1,43 +1,121 @@
 import unittest
 import requests
-#import json
 
 
 class UnitTestExample(unittest.TestCase):
 
     def setUp(self):
         """setting up test"""
-        pass
+        requests.delete("http://localhost:1234/books", timeout=5)
 
     def tearDown(self):
         """tearing down test
         only runs if setUp successful"""
-        pass
-
-    def test_book_retrieval(self):
-        """test retrieving the canned book in the sim"""
-        r = requests.get("http://localhost:1234/", timeout=5)
-
-        book = r.json()
-        #book = json.loads(r.text)
-
-        self.assertEqual(r.status_code, 200)
-        #new in python 2.7, auto calling of type specific equality function
-        self.assertEqual(book['identifier']['ISBN-10'], "0374530637")
-        self.assertEqual(book['identifier']['ISBN-13'], "978-0374530631")
-        self.assertEqual(book['identifier']['OCLC'], "256887668")
-        self.assertEqual(book['title'], "Wise Blood")
-        self.assertEqual(book['pages'], 238)
-        self.assertTrue(book['available'])
-        self.assertEqual(book['authors'], ["Flannery O'Connor"])
+        requests.delete("http://localhost:1234/books", timeout=5)
 
     def test_book_creation(self):
-        """test posting json to the sim and confirming response code"""
-        book = {'identifier': {'ISBN-10': "0374530874", 'ISBN-13': "978-0374530877", 'OCLC': "170140"}, 'title': "The Violent Bear It Away", 'pages': 243, 'available': True, 'authors': ["Flannery O'Connor"]}
-
-        r = requests.post("http://localhost:1234/", json=book, timeout=5)
+        """verify book creation"""
+        book = {'identifier': {'ISBN-10': "0374530874"}, 'title': "The Violent Bear It Away"}
+        r = requests.post("http://localhost:1234/books", json=book, timeout=5)
         self.assertEqual(r.status_code, 201)
-        #once sim can store books more assertions and cases will be added
+
+    def test_duplicate_book(self):
+        """attempt duplicate book creation"""
+        url = "http://localhost:1234/books"
+        book = {'identifier': {'ISBN-10': "0374530874"}, 'title': "The Violent Bear It Away"}
+        requests.post(url, json=book, timeout=5)
+        r = requests.post(url, json=book, timeout=5)
+        self.assertEqual(r.status_code, 409)
+
+    def test_book_same_title(self):
+        """verify creating multiple books with same title"""
+        url = "http://localhost:1234/books"
+        book = {'identifier': {'ISBN-10': "0374530874"}, 'title': "The Violent Bear It Away"}
+        requests.post(url, json=book, timeout=5)
+        book['identifier']['ISBN-10'] = "0374530875"
+        r = requests.post(url, json=book, timeout=5)
+        self.assertEqual(r.status_code, 201)
+
+    def test_book_retrieval(self):
+        """verify retrieving a book"""
+        url = "http://localhost:1234/books/"
+        book = {'identifier': {'ISBN-10': "0374530874"}, 'title': "The Violent Bear It Away"}
+        requests.post(url, json=book, timeout=5)
+        r = requests.get(url + book['identifier']['ISBN-10'], timeout=5)
+        res_body = r.json()
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(res_body['identifier']['ISBN-10'], book['identifier']['ISBN-10'])
+        self.assertEqual(res_body['title'], book['title'])
+
+    def test_nonexistent_book_retrieval(self):
+        """attempt to retrieve book that does not exist"""
+        r = requests.get("http://localhost:1234/books/idontexist", timeout=5)
+        self.assertEqual(r.status_code, 404)
+
+    def test_get_all_books_empty(self):
+        """retrieve all books when none exist"""
+        r = requests.get("http://localhost:1234/books", timeout=5)
+        self.assertEqual(r.status_code, 200)
+        self.assertFalse(r.json()['books'])
+
+    def test_retrieve_books(self):
+        """retrieve all books when some exist"""
+        url = "http://localhost:1234/books"
+        book = {'identifier': {'ISBN-10': "0374530874"}, 'title': "The Violent Bear It Away"}
+        requests.post(url, json=book, timeout=5)
+        book = {'identifier': {'ISBN-10': "0374530637"}, 'title': "Wise Blood"}
+        requests.post(url, json=book, timeout=5)        
+        r = requests.get(url, timeout=5)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(len(r.json()['books']), 2)
+
+    def test_nonexistent_book_removal(self):
+        """attempt to remove a book that does not exist"""
+        r = requests.delete("http://localhost:1234/books/idontexist", timeout=5)
+        self.assertEqual(r.status_code, 404)
+
+    def test_book_removal(self):
+        """verify removing a book"""
+        url = "http://localhost:1234/books/"
+        book = {'identifier': {'ISBN-10': "0374530874"}, 'title': "The Violent Bear It Away"}
+        requests.post(url, json=book, timeout=5)
+        r = requests.delete(url + book['identifier']['ISBN-10'], timeout=5)
+        self.assertEqual(r.status_code, 200)
+        r = requests.get(url + book['identifier']['ISBN-10'], timeout=5)
+        self.assertEqual(r.status_code, 404)
+
+    def test_remove_all_books_empty(self):
+        """remove all on empty library"""
+        r = requests.delete("http://localhost:1234/books", timeout=5)
+        self.assertEqual(r.status_code, 200)
+        
+    def test_remove_all_books_some_exist(self):
+        """remove all books when some exist"""
+        url = "http://localhost:1234/books"
+        book = {'identifier': {'ISBN-10': "0374530874"}, 'title': "The Violent Bear It Away"}
+        requests.post(url, json=book, timeout=5)
+        book = {'identifier': {'ISBN-10': "0374530637"}, 'title': "Wise Blood"}
+        requests.post(url, json=book, timeout=5)        
+        r = requests.delete(url, timeout=5)
+        self.assertEqual(r.status_code, 200)
+        r = requests.get(url, timeout=5)
+        self.assertFalse(r.json()['books'])
+
+    def test_book_creation_invalid_json(self):
+        """attempt to create a book with invalid json"""
+        invalid_json = [{'identifier': {'ISBN-13': "978-0374530631"}, 'title': "Wise Blood"},
+                        {'identifier': {'ISBN-10': None}, 'title': "Wise Blood"},
+                        {'identifier': {'ISBN-10': "         "}, 'title': "Wise Blood"},
+                        {'identifier': {'ISBN-10': 3745306370}, 'title': "Wise Blood"},
+                        {'title': "Wise Blood"},
+                        {'identifier': {'ISBN-10': "0374530637"}},
+                        {'identifier': {'ISBN-10': "0374530637"}, 'title': None},
+                        {'identifier': {'ISBN-10': "0374530637"}, 'title': ""},
+                        {'identifier': {'ISBN-10': "0374530637"}, 'title': []}]
+        for payload in invalid_json:
+            r = requests.post("http://localhost:1234/books", json=payload, timeout=5)
+            self.assertEqual(r.status_code, 400)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
