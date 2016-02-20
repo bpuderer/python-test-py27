e53b484f64c6934ba56ca21cde501df89fbe4279
Ben Puderer <bpuderer@yahoo.com>
2016-02-20 09:20:28 -0500

    added update, errorhandlers, book with uri

--- flask_http_sim.py
+++ flask_http_sim.py
@@ -1,26 +1,38 @@
 import argparse
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, jsonify, abort, url_for, make_response
+
 app = Flask(__name__)
+books = []
 
-library = {'books': []}
 
+def make_public_book(book):
+    new_book = {}
+    for field in book:
+        new_book[field] = book[field]
+    new_book['uri'] = url_for('get_book', bookid=book['identifier']['ISBN-10'], _external=True)
+    return new_book
 
-def book_location(isbn10):
-    for i, book in enumerate(library['books']):
-        if book['identifier']['ISBN-10'] == isbn10:
-            return i
-    return -1
+@app.errorhandler(404)
+def not_found(e):
+    return make_response(jsonify({'error': 'Book not found'}), 404)
 
+@app.errorhandler(400)
+def bad_request(e):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+@app.errorhandler(409)
+def conflict(e):
+    return make_response(jsonify({'error': 'Book already exists'}), 409)
 
 @app.route('/books', methods=['GET'])
 def get_books():
-    return jsonify(library)
+    return jsonify({'books': [make_public_book(book) for book in books]})
 
 @app.route('/books/<bookid>', methods=['GET'])
 def get_book(bookid):
-    loc = book_location(bookid)
-    if loc > -1:
-        return jsonify(library['books'][loc])
+    book = [book for book in books if book['identifier']['ISBN-10'] == bookid]
+    if book:
+        return jsonify(book[0])
     else:
         abort(404)
 
@@ -30,27 +42,47 @@ def create_book():
         abort(400)
     elif 'identifier' not in request.json or 'ISBN-10' not in request.json['identifier'] or 'title' not in request.json:
         abort(400)
+    #better to use duck typing
     elif type(request.json['identifier']['ISBN-10']) != unicode or type(request.json['title']) != unicode:
         abort(400)
     elif not request.json['identifier']['ISBN-10'].strip() or not request.json['title'].strip():
         abort(400)
-    elif book_location(request.json['identifier']['ISBN-10']) > -1:
+    elif [book for book in books if book['identifier']['ISBN-10'] == request.json['identifier']['ISBN-10']]:
         abort(409)
     else:
-        library['books'].append(request.json)
-        return jsonify(request.json), 201
+        books.append(request.json)
+        return jsonify(make_public_book(request.json)), 201
+
+@app.route('/books/<bookid>', methods=['PUT'])
+def update_book(bookid):
+    if not request.json:
+        abort(400)
+    elif 'identifier' not in request.json or 'ISBN-10' not in request.json['identifier'] or 'title' not in request.json:
+        abort(400)
+    #better to use duck typing
+    elif type(request.json['identifier']['ISBN-10']) != unicode or type(request.json['title']) != unicode:
+        abort(400)
+    elif not request.json['identifier']['ISBN-10'].strip() or not request.json['title'].strip():
+        abort(400)
+
+    book = [book for book in books if book['identifier']['ISBN-10'] == bookid]
+    if not book:
+        abort(404)
+    book[0]['title'] = request.json['title']
+    book[0]['identifier']['ISBN-10'] = request.json['identifier']['ISBN-10']
+    return jsonify(make_public_book(book[0]))
 
 @app.route('/books', methods=['DELETE'])
 def delete_books():
-    del library['books'][:]
-    return ('', 200)
+    del books[:]
+    return jsonify({'result': True})
 
 @app.route('/books/<bookid>', methods=['DELETE'])
 def delete_book(bookid):
-    loc = book_location(bookid)
-    if loc > -1:
-        del library['books'][loc]
-        return ('', 200)
+    book = [book for book in books if book['identifier']['ISBN-10'] == bookid]
+    if book:
+        books.remove(book[0])
+        return jsonify({'result': True})
     else:
         abort(404)
 
