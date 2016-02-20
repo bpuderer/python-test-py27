97b96380782948d8d221df5c8f72a9029c2c1478
Ben Puderer <bpuderer@yahoo.com>
2016-02-20 14:29:31 -0500

    use httlib constants for response code assertions

--- unittest_demo/test_example.py
+++ unittest_demo/test_example.py
@@ -1,3 +1,4 @@
+import httplib
 import unittest
 import requests
 
@@ -17,7 +18,7 @@ class UnitTestExample(unittest.TestCase):
         """verify book creation"""
         book = {'identifier': {'ISBN-10': "0374530874"}, 'title': "The Violent Bear It Away"}
         r = requests.post("http://localhost:1234/books", json=book, timeout=5)
-        self.assertEqual(r.status_code, 201)
+        self.assertEqual(r.status_code, httplib.CREATED)
 
     def test_duplicate_book(self):
         """attempt duplicate book creation"""
@@ -25,7 +26,7 @@ class UnitTestExample(unittest.TestCase):
         book = {'identifier': {'ISBN-10': "0374530874"}, 'title': "The Violent Bear It Away"}
         requests.post(url, json=book, timeout=5)
         r = requests.post(url, json=book, timeout=5)
-        self.assertEqual(r.status_code, 409)
+        self.assertEqual(r.status_code, httplib.CONFLICT)
 
     def test_book_same_title(self):
         """verify creating multiple books with same title"""
@@ -34,7 +35,7 @@ class UnitTestExample(unittest.TestCase):
         requests.post(url, json=book, timeout=5)
         book['identifier']['ISBN-10'] = "0374530875"
         r = requests.post(url, json=book, timeout=5)
-        self.assertEqual(r.status_code, 201)
+        self.assertEqual(r.status_code, httplib.CREATED)
 
     def test_book_retrieval(self):
         """verify retrieving a book"""
@@ -43,19 +44,19 @@ class UnitTestExample(unittest.TestCase):
         requests.post(url, json=book, timeout=5)
         r = requests.get(url + '/' + book['identifier']['ISBN-10'], timeout=5)
         res_body = r.json()
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.status_code, httplib.OK)
         self.assertEqual(res_body['identifier']['ISBN-10'], book['identifier']['ISBN-10'])
         self.assertEqual(res_body['title'], book['title'])
 
     def test_nonexistent_book_retrieval(self):
         """attempt to retrieve book that does not exist"""
         r = requests.get("http://localhost:1234/books/idontexist", timeout=5)
-        self.assertEqual(r.status_code, 404)
+        self.assertEqual(r.status_code, httplib.NOT_FOUND)
 
     def test_get_all_books_empty(self):
         """retrieve all books when none exist"""
         r = requests.get("http://localhost:1234/books", timeout=5)
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.status_code, httplib.OK)
         self.assertFalse(r.json()['books'])
 
     def test_retrieve_books(self):
@@ -66,13 +67,13 @@ class UnitTestExample(unittest.TestCase):
         book = {'identifier': {'ISBN-10': "0374530637"}, 'title': "Wise Blood"}
         requests.post(url, json=book, timeout=5)        
         r = requests.get(url, timeout=5)
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.status_code, httplib.OK)
         self.assertEqual(len(r.json()['books']), 2)
 
     def test_nonexistent_book_removal(self):
         """attempt to remove a book that does not exist"""
         r = requests.delete("http://localhost:1234/books/idontexist", timeout=5)
-        self.assertEqual(r.status_code, 404)
+        self.assertEqual(r.status_code, httplib.NOT_FOUND)
 
     def test_book_removal(self):
         """verify removing a book"""
@@ -80,14 +81,14 @@ class UnitTestExample(unittest.TestCase):
         book = {'identifier': {'ISBN-10': "0374530874"}, 'title': "The Violent Bear It Away"}
         requests.post(url, json=book, timeout=5)
         r = requests.delete(url + '/' + book['identifier']['ISBN-10'], timeout=5)
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.status_code, httplib.OK)
         r = requests.get(url + '/' + book['identifier']['ISBN-10'], timeout=5)
-        self.assertEqual(r.status_code, 404)
+        self.assertEqual(r.status_code, httplib.NOT_FOUND)
 
     def test_remove_all_books_empty(self):
         """remove all on empty library"""
         r = requests.delete("http://localhost:1234/books", timeout=5)
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.status_code, httplib.OK)
         
     def test_remove_all_books_some_exist(self):
         """remove all books when some exist"""
@@ -97,7 +98,7 @@ class UnitTestExample(unittest.TestCase):
         book = {'identifier': {'ISBN-10': "0374530637"}, 'title': "Wise Blood"}
         requests.post(url, json=book, timeout=5)        
         r = requests.delete(url, timeout=5)
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.status_code, httplib.OK)
         r = requests.get(url, timeout=5)
         self.assertFalse(r.json()['books'])
 
@@ -114,7 +115,7 @@ class UnitTestExample(unittest.TestCase):
                         {'identifier': {'ISBN-10': "0374530637"}, 'title': []}]
         for payload in invalid_json:
             r = requests.post("http://localhost:1234/books", json=payload, timeout=5)
-            self.assertEqual(r.status_code, 400)
+            self.assertEqual(r.status_code, httplib.BAD_REQUEST)
 
 
 if __name__ == '__main__':
