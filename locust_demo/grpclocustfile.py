f4e42d6bd5a6de76ae3c6e3ee95c36b6bc73f1d1
Ben Puderer <bpuderer@yahoo.com>
2018-05-12 09:39:58 -0400

    cleanup

--- locust_demo/grpclocustfile.py
+++ locust_demo/grpclocustfile.py
@@ -1,13 +1,18 @@
 import time
 
 import grpc
+from google.protobuf import json_format
 from locust import Locust, TaskSet, events, task
 
-from doubler_pb2 import Number
-import doubler_pb2_grpc
+from grpc_request import doubler_pb2, doubler_pb2_grpc
 
 
 class UserBehavior(TaskSet):
+
+    def on_start(self):
+        with open("./grpc_request/request.json") as f:
+            json_str = f.read()
+        self.request = json_format.Parse(json_str, doubler_pb2.Number())
 
     @task
     def make_call(self):
@@ -15,7 +20,7 @@ class UserBehavior(TaskSet):
 
         start_time = time.time()
         try:
-            self.client.Double(Number(value=3.0), timeout=1)
+            self.client.Double(self.request, timeout=1)
         except Exception as e:
             total_time = int((time.time() - start_time) * 1000)
             events.request_failure.fire(request_type="grpc", name="doubler", response_time=total_time, exception=e)
