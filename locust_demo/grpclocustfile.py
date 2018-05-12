5a5a980a47acd840f9d43b100db439b91c0e9c0d
Ben Puderer <bpuderer@yahoo.com>
2018-05-12 18:23:16 -0400

    better grpc locustfile

--- locust_demo/grpclocustfile.py
+++ locust_demo/grpclocustfile.py
@@ -1,15 +1,20 @@
 import time
 
-import grpc
+from grpc import insecure_channel
 from google.protobuf import json_format
 from locust import Locust, TaskSet, events, task
+from locust.exception import LocustError
 
 from grpc_request import doubler_pb2, doubler_pb2_grpc
+
 
 
 class UserBehavior(TaskSet):
 
     def on_start(self):
+        self.read_request()
+
+    def read_request(self):
         with open("./grpc_request/request.json") as f:
             json_str = f.read()
         self.request = json_format.Parse(json_str, doubler_pb2.Number())
@@ -29,11 +34,16 @@ class UserBehavior(TaskSet):
             events.request_success.fire(request_type="grpc", name="doubler", response_time=total_time, response_length=0)
 
 
-class GrpcUser(Locust):
+class GrpcLocust(Locust):
+    def __init__(self):
+        super(GrpcLocust, self).__init__()
+        if self.host is None:
+            raise LocustError("Host not specified")
+        self.client = doubler_pb2_grpc.DoublerStub(insecure_channel(self.host))
 
+
+class ApiUser(GrpcLocust):
+    host = 'localhost:50051'
     task_set = UserBehavior
     min_wait = 300
     max_wait = 1000
-
-    channel = grpc.insecure_channel('localhost:50051')
-    client = doubler_pb2_grpc.DoublerStub(channel)
